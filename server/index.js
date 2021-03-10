5819db3166e3adc22e3b8a5573a6eab631a57e24
rsarkis23 <learning.r2@gmail.com>
2021-03-09 23:24:07 -0500

    finished code

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const getProducts = require('./getProducts')
+const getProduct = require('./getProduct')
+
+const app = express()
+
+const port = 3333
+
+app.get('/api/products', getProducts)
+app.get('/api/products/:id', getProduct)
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port: ${ port }`)
+})
